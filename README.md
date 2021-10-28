9c6944cd027df5b1ebd9664b417bc6afbfb0d10c
DViga <42376959+DVigas@users.noreply.github.com>
2021-10-28 21:32:43 +0100

    update
    update

--- README.md
+++ README.md
@@ -10,22 +10,19 @@
 * The page looks for information of the character chosen by the user
 
 ---
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+
 tibia_api_js_bootstrap
 
-Consuming Tibia MMORPG API. Character search - JS and Bootstrap
+## Consuming Tibia MMORPG API. Character search - JS and Bootstrap
 
 ***
 
 Print screen of usage examples:
 
 
-<img src="source/img/screen01.png" alt="teste" width="350em">
+<img src="source/img/screen01.png" alt="First Screenshot" width="500em">
 
-<img src="source/img/screen02.png" alt="teste" width="350em">
-
-
-![Screen 01](/source/img/screen01.png)
-
-![Screen 01](/source/img/screen02.png)
+<img src="source/img/screen02.png" alt="Second Screenshot" width="500em">
 
 
--- source/script.js
+++ source/script.js
@@ -1,91 +1,56 @@
-var charName = document.getElementById("txtChar");
-var charContainer = document.getElementById("mostrar-info");
-var btn = document.getElementById("btn");
-
+var charName = document.getElementById('txtChar')
+var charContainer = document.getElementById('show-info')
+var btn = document.getElementById('btn')
 
 // when press the button: //
-btn.addEventListener("click", function(){
-    
-    document.getElementById("mostrar-info").innerHTML = "";    
+btn.addEventListener('click', function () {
+  document.getElementById('show-info').innerHTML = ''
 
+  var ourRequest = new XMLHttpRequest()
 
-    var ourRequest = new XMLHttpRequest();    
-    
+  ourRequest.open(
+    'GET',
+    'https://api.tibiadata.com/v2/characters/' + charName.value + '.json'
+  ) // se eu quiser enviar requisicao coloco POST em vez de GET
 
-    ourRequest.open('GET', 'https://api.tibiadata.com/v2/characters/' + charName.value + '.json');  // se eu quiser enviar requisicao coloco POST em vez de GET
+  ourRequest.onload = function () {
+    var ourData = JSON.parse(ourRequest.responseText)
+    //console.log(ourData.characters.data);
+    //renderHTML(ourData);
 
-    ourRequest.onload = function(){
-        var ourData = JSON.parse(ourRequest.responseText);
-        //console.log(ourData.characters.data);
-        //renderHTML(ourData);
+    var htmlString = ''
+    htmlString += '<p> Name: ' + ourData.characters.data.name + '</p>'
+    htmlString += '<p> Vocation: ' + ourData.characters.data.vocation + '</p>'
+    htmlString += '<p> Level: ' + ourData.characters.data.level + '</p>'
+    htmlString += '<p> World: ' + ourData.characters.data.world + '</p>'
+    htmlString += '<p> Residence: ' + ourData.characters.data.residence + '</p>'
+    htmlString +=
+      '<p> Achievement Points: ' +
+      ourData.characters.data.achievement_points +
+      '</p>'
+    htmlString +=
+      '<p> Account status: ' + ourData.characters.data.account_status + '</p>'
+    htmlString +=
+      '<p> <strong> Status: </strong> ' +
+      ourData.characters.data.status +
+      '</p>'
 
-        var htmlString = "";
-        htmlString += "<p> Nome: " + ourData.characters.data.name + "</p>";
-        htmlString += "<p> Vocacao: " + ourData.characters.data.vocation + "</p>";
-        htmlString += "<p> Nivel: " + ourData.characters.data.level + "</p>";
-        htmlString += "<p> Mundo: " + ourData.characters.data.world + "</p>";
-        htmlString += "<p> Residencia: " + ourData.characters.data.residence + "</p>";
-        htmlString += "<p> Pontos de conquistas: " + ourData.characters.data.achievement_points + "</p>";
-        htmlString += "<p> Status da Conta: " + ourData.characters.data.account_status + "</p>";
-        htmlString += "<p> <strong> Status: </strong> " + ourData.characters.data.status + "</p>";
+    var otherChars = ourData.characters.other_characters
 
-        var otherChars = ourData.characters.other_characters;
+    for (i = 0; i < otherChars.length; i++) {
+      htmlString +=
+        '<p> Nome: ' +
+        otherChars[i].name +
+        ' - <strong> Status: </strong>' +
+        otherChars[i].status +
+        '</p>'
+      //htmlString += "<p> Status: " + otherChars[i].status + "</p>";
+    }
 
-        for (i=0; i < otherChars.length; i++){
-            htmlString += "<p> Nome: " + otherChars[i].name + " - <strong> Status: </strong>" + otherChars[i].status + "</p>";
-            //htmlString += "<p> Status: " + otherChars[i].status + "</p>";    
-        }
+    charContainer.insertAdjacentHTML('beforeend', htmlString)
 
-        
-        charContainer.insertAdjacentHTML('beforeend', htmlString); 
+    console.log(otherChars[0].name)
+  }
 
-        console.log(otherChars[0].name);
-       
-    };
-
-    ourRequest.send();
-
-});
-
-
-
-// funcao para adicionar html para a pagina para o div elemento 
-// function renderHTML(data){
-//     var htmlString = "";
-
-//     htmlString += "<p>" + data[i].name + ".</p>";
-    
-
-    // for (i=0; i < data.length; i++){
-    //     htmlString += "<p>" + data[i].name + ".</p>";
-        
-    // }
-
-    //charContainer.insertAdjacentHTML('beforeend', htmlString);
-
-// }
-
-
-// =============== ******************** ============= //
-// SO ESSE CODIGO ABAIXO JA FUNCIONA NO CONSOLE, O CODIGO ACIMA E PARA EXIBIR NO BROWSER COM AJAX //
-
-// var ourRequest = new XMLHttpRequest();
-// var charName = 'jviga';
-
-// ourRequest.open('GET', 'https://api.tibiadata.com/v2/characters/' + charName + '.json');  // se eu quiser enviar requisicao coloco POST em vez de GET
-
-// ourRequest.onload = function(){
-//     var ourData = JSON.parse(ourRequest.responseText);
-//     //console.log(ourData.characters.data);
-
-//      console.log(ourData.characters.data.name);
-//      console.log(ourData.characters.data.vocation);
-//      console.log(ourData.characters.data.level);
-//      console.log(ourData.characters.data.world);
-//      console.log(ourData.characters.data.residence);
-//      console.log(ourData.characters.data.achievement_points);
-//      console.log(ourData.characters.data.account_status);
-//      console.log(ourData.characters.data.status);
-// };
-
-// ourRequest.send();
+  ourRequest.send()
+})
